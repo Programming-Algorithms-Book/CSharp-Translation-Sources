06100bb052f3945b8db22d72796b0449f2e81e27
Teodor92 <teodor.ivanov92@gmail.com>
2015-06-12 15:40:24 +0300

    Code style importations to chapter 4

--- Chapter 4/BinarySearch.Iterative/BinarySearchIterative.cs
+++ Chapter 4/BinarySearch.Iterative/BinarySearchIterative.cs
@@ -1,100 +1,117 @@
-﻿using System;
-using System.Text;
-
-class BinarySearchIterative
+﻿namespace BinarySearch.Iterative
 {
-    public class Element<T>
-    {
-        public int Key { get; set; }
-        public T Value { get; set; }
+    using System;
+    using System.Text;
 
-        public Element(int key, T value)
+    public class BinarySearchIterative
+    {
+        // Максимална стойност на елементите от масива.
+        private const int MaxValue = 50;
+        private const int NotFound = -1;
+
+        // Генератор на произволни числа.
+        private static readonly Random Random = new Random();
+
+        public static void PerformSearchTest(Element<int>[] elements, int n)
         {
-            this.Key = key;
-            this.Value = value;
-        }
-    }
-
-    static Random random = new Random();         // Генератор на произволни числа.
-    const int MaxValue = 50;                    // Максимална стойност на елементите от масива.
-	const int NotFound = -1;
-
-    static void SortElementsArray(Element<int>[] elements)        // Сортира елементите в масива.
-    {
-        for (int i = 0; i < elements.Length; i++)
-            for (int j = 0; j < elements.Length; j++)
-                if (elements[i].Key < elements[j].Key)
-                {
-                    var swapElement = elements[i];
-                    elements[i] = elements[j];
-                    elements[j] = swapElement;
-                }
-    }
-
-    static int BinarySearch(Element<int>[] elements, int elementToSearch)     // Извърша двоично търсене.
-    {
-        int leftIndex = 0;
-        int rightIndex = elements.Length - 1;
-        int midIndex;
-        int result = NotFound;
-        while (leftIndex <= rightIndex)
-        {
-            midIndex = (leftIndex + rightIndex) / 2;
-            if (elementToSearch < elements[midIndex].Key)
-                rightIndex = midIndex - 1;
-            else if (elementToSearch > elements[midIndex].Key)
-                leftIndex = midIndex + 1;
-            else
+            for (int elementToSearch = 0; elementToSearch < 2 * MaxValue; elementToSearch++)
             {
-                result = midIndex;
-                break;
+                Console.WriteLine("Търсим елемент с ключ {0}.", elementToSearch);
+                var index = BinarySearch(elements, elementToSearch);
+                if (index == NotFound)
+                {
+                    Console.WriteLine("Елемент с такъв ключ не съществува!");
+                }
+                else
+                {
+                    Console.WriteLine("Елементът е намерен! Стойност на инф. част: {0}.", index);
+                }
             }
         }
-        return result;
-    }
 
-    static void PrintElements(Element<int>[] elements)        // Принтира елементите на масива върху конзолата.
-    {
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < elements.Length; i++)
+        internal static void Main()
         {
-            output.Append(elements[i].Value);
-            if (i < elements.Length - 1)
-                output.Append(",");
-        }
-        Console.WriteLine(output.ToString());
-    }
+            // Брой елементи в масива.
+            int n = MaxValue;
 
-    public static void PerformSearchTest(Element<int>[] elements, int n)
-    {
-        int index;
-        for (int elementToSearch = 0; elementToSearch < 2 * MaxValue; elementToSearch++)
-        {
-            Console.WriteLine("Търсим елемент с ключ {0}.", elementToSearch);
-            index = BinarySearch(elements, elementToSearch);
-            if (index == NotFound)
-                Console.WriteLine("Елемент с такъв ключ не съществува!");
-            else
-                Console.WriteLine("Елементът е намерен! Стойност на инф. част: {0}.", index);
-        }
-    }
+            // Инициализация на масив със записи.            
+            Element<int>[] elements = new Element<int>[n];
+            for (int index = 0; index < n; index++)
+            {
+                int randomNumber = Random.Next(0, 2 * MaxValue);
 
-    static void Main()
-    {
-        int n = MaxValue;                                                    // Брой елементи в масива.
-        Element<int>[] elements = new Element<int>[n];                  // Инициализация на масив със записи.
-        for (int index = 0; index < n; index++)
-        {
-            int randomNumber = random.Next(0, 2 * MaxValue);
-            elements[index] = new Element<int>(randomNumber, index);     // Пълнене на масива с произволни числа.
+                // Пълнене на масива с произволни числа.
+                elements[index] = new Element<int>(randomNumber, index);
+            }
+
+            SortElementsArray(elements);
+
+            Console.WriteLine("Списъкът съдържа следните елементи: ");
+            PrintElements(elements);
+
+            Console.WriteLine("\nТестване:");
+            PerformSearchTest(elements, n);
         }
 
-        SortElementsArray(elements);
+        // Сортира елементите в масива.
+        private static void SortElementsArray(Element<int>[] elements)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    if (elements[i].Key < elements[j].Key)
+                    {
+                        var swapElement = elements[i];
+                        elements[i] = elements[j];
+                        elements[j] = swapElement;
+                    }
+                }
+            }
+        }
 
-        Console.WriteLine("Списъкът съдържа следните елементи: ");
-        PrintElements(elements);
+        // Извърша двоично търсене.
+        private static int BinarySearch(Element<int>[] elements, int elementToSearch)
+        {
+            int leftIndex = 0;
+            int rightIndex = elements.Length - 1;
+            int midIndex;
+            int result = NotFound;
+            while (leftIndex <= rightIndex)
+            {
+                midIndex = (leftIndex + rightIndex) / 2;
+                if (elementToSearch < elements[midIndex].Key)
+                {
+                    rightIndex = midIndex - 1;
+                }
+                else if (elementToSearch > elements[midIndex].Key)
+                {
+                    leftIndex = midIndex + 1;
+                }
+                else
+                {
+                    result = midIndex;
+                    break;
+                }
+            }
 
-        Console.WriteLine("\nТестване:");
-        PerformSearchTest(elements, n);
+            return result;
+        }
+
+        // Принтира елементите на масива върху конзолата.
+        private static void PrintElements(Element<int>[] elements)
+        {
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < elements.Length; i++)
+            {
+                output.Append(elements[i].Value);
+                if (i < elements.Length - 1)
+                {
+                    output.Append(",");
+                }
+            }
+
+            Console.WriteLine(output.ToString());
+        }
     }
 }
--- Chapter 4/InterpolSearch/InterpolSearch.cs
+++ Chapter 4/InterpolSearch/InterpolSearch.cs
@@ -1,81 +1,15 @@
-﻿using System;
-using System.Text;
+﻿namespace InterpolSearch
+{
+    using System;
+    using System.Text;
 
-class InterpolationSearch
+    public class InterpolationSearch
     {
-        public class Element<T>
-        {
-            public int Key { get; set; }
-            public T Value { get; set; }
+        private const int MaxValue = 50;              // Максимална стойност на елементите от масива.
+        private const int NotFound = -1;
 
-            public Element(int key, T value)
-            {
-                this.Key = key;
-                this.Value = value;
-            }
-
-            public override string ToString()
-            {
-                return this.Value.ToString();
-            }
-        }
-
-        static Random random = new Random();         // Генератор на произволни числа.
-        const int MaxValue = 50;              // Максимална стойност на елементите от масива.
-        const int NotFound = -1;
-        static int[] fibunacciNumbers = new int[MaxValue]; // Числата на Фибоначи, ненадвишаващи n
-
-        static void SortElementsArray(Element<int>[] elements)        // Сортира елементите в масива.
-        {
-            for (int i = 0; i < elements.Length; i++)
-                for (int j = 0; j < elements.Length; j++)
-                    if (elements[i].Key < elements[j].Key)
-                    {
-                        var swapElement = elements[i];
-                        elements[i] = elements[j];
-                        elements[j] = swapElement;
-                    }
-        }
-
-        static int InterpolSearch(Element<int>[] elements, int keyToSearch)
-        {
-            int leftIndex, rightIndex, midIndex;
-            float interpolCoefficent;
-            leftIndex = 0;
-            rightIndex = elements.Length - 1;
-            while (leftIndex <= rightIndex)
-            {
-                if (elements[rightIndex].Key == elements[leftIndex].Key)
-                    if (elements[leftIndex].Key == keyToSearch)
-                        return leftIndex;
-                    else
-                        return NotFound;
-                interpolCoefficent = (float)(keyToSearch - elements[leftIndex].Key) 
-                    / (elements[rightIndex].Key - elements[leftIndex].Key);
-                if (interpolCoefficent < 0 || interpolCoefficent > 1)
-                    return NotFound;
-                midIndex = (int)(leftIndex + interpolCoefficent * (rightIndex - leftIndex) + 0.5);
-                if (keyToSearch < elements[midIndex].Key)
-                    rightIndex = midIndex - 1;
-                else if (keyToSearch > elements[midIndex].Key)
-                    leftIndex = midIndex + 1;
-                else
-                    return midIndex;
-            }
-            return NotFound;
-        }
-
-        static void PrintElements(Element<int>[] elements)        // Принтира елементите на масива върху конзолата.
-        {
-            StringBuilder output = new StringBuilder();
-            for (int i = 0; i < elements.Length; i++)
-            {
-                output.Append(elements[i].ToString());
-                if (i < elements.Length - 1)
-                    output.Append(",");
-            }
-            Console.WriteLine(output.ToString());
-        }
+        private static readonly Random Random = new Random();         // Генератор на произволни числа.
+        private static int[] fibunacciNumbers = new int[MaxValue]; // Числата на Фибоначи, ненадвишаващи n
 
         public static void PerformSearchTest(Element<int>[] elements)
         {
@@ -85,18 +19,22 @@ class InterpolationSearch
                 Console.WriteLine("Търсим елемент с ключ {0}.", elementToSearch);
                 index = InterpolSearch(elements, elementToSearch);
                 if (index == -1)
+                {
                     Console.WriteLine("Елемент с такъв ключ не съществува!");
+                }
                 else
+                {
                     Console.WriteLine("Елементът е намерен! Стойност на инф. част: {0}.", index);
+                }
             }
         }
 
-        static void Main()
+        internal static void Main()
         {                                                    // Брой елементи в масива.
             Element<int>[] elements = new Element<int>[MaxValue];                  // Инициализация на масив със записи.
             for (int index = 0; index < MaxValue; index++)
             {
-                int randomNumber = random.Next(0, 2 * MaxValue);
+                int randomNumber = Random.Next(0, 2 * MaxValue);
                 elements[index] = new Element<int>(randomNumber, index);     // Пълнене на масива с произволни числа.
             }
 
@@ -108,5 +46,81 @@ class InterpolationSearch
             Console.WriteLine("\nТестване:");
             PerformSearchTest(elements);
         }
-    }
 
+        private static void SortElementsArray(Element<int>[] elements)        // Сортира елементите в масива.
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    if (elements[i].Key < elements[j].Key)
+                    {
+                        var swapElement = elements[i];
+                        elements[i] = elements[j];
+                        elements[j] = swapElement;
+                    }
+                }
+            }
+        }
+
+        private static int InterpolSearch(Element<int>[] elements, int keyToSearch)
+        {
+            int leftIndex, rightIndex, midIndex;
+            float interpolCoefficent;
+            leftIndex = 0;
+            rightIndex = elements.Length - 1;
+            while (leftIndex <= rightIndex)
+            {
+                if (elements[rightIndex].Key == elements[leftIndex].Key)
+                {
+                    if (elements[leftIndex].Key == keyToSearch)
+                    {
+                        return leftIndex;
+                    }
+                    else
+                    {
+                        return NotFound;
+                    }
+                }
+
+                interpolCoefficent = (float)(keyToSearch - elements[leftIndex].Key)
+                                     / (elements[rightIndex].Key - elements[leftIndex].Key);
+                if (interpolCoefficent < 0 || interpolCoefficent > 1)
+                {
+                    return NotFound;
+                }
+
+                midIndex = (int)(leftIndex + (interpolCoefficent * (rightIndex - leftIndex)) + 0.5);
+                if (keyToSearch < elements[midIndex].Key)
+                {
+                    rightIndex = midIndex - 1;
+                }
+                else if (keyToSearch > elements[midIndex].Key)
+                {
+                    leftIndex = midIndex + 1;
+                }
+                else
+                {
+                    return midIndex;
+                }
+            }
+
+            return NotFound;
+        }
+
+        private static void PrintElements(Element<int>[] elements)        // Принтира елементите на масива върху конзолата.
+        {
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < elements.Length; i++)
+            {
+                output.Append(elements[i].ToString());
+                if (i < elements.Length - 1)
+                {
+                    output.Append(",");
+                }
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
